79cef159298dfea679975165536d03fbe563cdc0
alevur <alevure@yandex.ru>
2023-03-07 20:37:37 +0400

    dzphp

--- PHP7).php
+++ PHP7).php
@@ -1,4 +1,7 @@
 <?php
 function displayInfo ($name,$address,$age){
-
+    echo "<div>Имя: {$name}<br>Имя: {$address}<br>адрес: {$age} </div><hr>";
 }
+displayInfo("BOB", 48975, 30 );
+displayInfo("JON",48976 ,27 );
+
